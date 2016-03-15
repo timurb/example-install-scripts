7acfbcd28310fb7c13f17249e4f8b8049a17dc06
Timur Batyrshin <erthad@gmail.com>
2016-03-15 23:55:51 +0300

    minor fixes to install-nginx; add handling of config files to nginx

--- install-nginx.sh
+++ install-nginx.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
+PWD="$(dirname $0)"
+
 sudo yum install -y epel-release
 sudo yum install -y nginx
 
 sudo systemctl enable nginx
 
-if which firewall-cmd > /dev/null; then
+if which firewall-cmd 2> /dev/null; then
   sudo firewall-cmd --permanent --zone=public --add-service=http 
   sudo firewall-cmd --permanent --zone=public --add-service=https
   sudo firewall-cmd --reload
-fi 
+fi
+
+cp -f "${PWD}/templates/nginx.conf" /etc/nginx/nginx.conf
+cp -f "${PWD}/templates/nginx-site.conf" /etc/nginx/conf.d/web-server.conf
+
+sudo systemctl start nginx
