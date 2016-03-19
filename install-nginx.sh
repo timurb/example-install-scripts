dde2e6804306c4ae31c7b870a5af773f076c506d
Timur Batyrshin <erthad@gmail.com>
2016-03-19 18:51:59 +0000

    don't let installation run under regular user

--- install-nginx.sh
+++ install-nginx.sh
@@ -1,19 +1,26 @@
 #!/bin/sh -e
 
+fail() (
+  echo "ERROR: $*" >&2
+  exit 1
+)
+
 PWD="$(dirname $0)"
 
-sudo yum install -y epel-release
-sudo yum install -y nginx
+[ "$(whoami)" != 'root' ] && fail "Run as 'root' user"
 
-sudo systemctl enable nginx
+yum install -y epel-release
+yum install -y nginx
+
+systemctl enable nginx
 
 if which firewall-cmd 2> /dev/null; then
-  sudo firewall-cmd --permanent --zone=public --add-service=http 
-  sudo firewall-cmd --permanent --zone=public --add-service=https
-  sudo firewall-cmd --reload
+  firewall-cmd --permanent --zone=public --add-service=http 
+  firewall-cmd --permanent --zone=public --add-service=https
+  firewall-cmd --reload
 fi
 
-sudo "${PWD}/gen-template/gen-template.py" --force --template "${PWD}/templates/nginx.conf.tpl" --json tomcat-proxy.json --out /etc/nginx/nginx.conf
-sudo "${PWD}/gen-template/gen-template.py" --force --template "${PWD}/templates/nginx-site.conf.tpl" --json tomcat-proxy.json --out /etc/nginx/conf.d/web-server.conf
+"${PWD}/gen-template/gen-template.py" --force --template "${PWD}/templates/nginx.conf.tpl" --json tomcat-proxy.json --out /etc/nginx/nginx.conf
+"${PWD}/gen-template/gen-template.py" --force --template "${PWD}/templates/nginx-site.conf.tpl" --json tomcat-proxy.json --out /etc/nginx/conf.d/web-server.conf
 
-sudo systemctl start nginx
+systemctl start nginx
